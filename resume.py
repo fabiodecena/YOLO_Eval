5d8f9edb1d532022cab4d5bdac83b6d05dfd27a8
fabio <fabiodecena@gmail.com>
2026-04-10 17:18:15 +0200

    Refined pipeline for the statistical analysis and performed R analysis from the two files.R!!!

--- resume.py
+++ resume.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 def resume_training():
-    # Point DIRECTLY to the 'last.pt' in your runs folder
+    # Point DIRECTLY to the 'last.pt'
     # This file contains the architecture, weights, and the epoch counter
     model_path = r'C:\Users\fabio\PycharmProjects\YOLO_Eval\runs\detect\flir_thermal_yolo11\weights\last.pt'
 
